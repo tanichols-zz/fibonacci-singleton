d0a648c13f17c7e64437539dfc4f4f2a106d26e5
Thomas Nichols <ta.nichols@outlook.com>
2018-11-20 10:10:44 -0600

    Added initial implementation

---
+++ index.js
@@ -0,0 +1,37 @@
+class Fibonacci {
+  constructor(size = 2) {
+    this.iterations = 0
+    this.size = 2
+    this.prior = 0
+    this.current = 1
+    this.sequence = [0, 1]
+    this.setSize(size)
+  }
+
+  setSize(size) {
+    if (this.size < size) {
+      this.size = size
+      this.compute()
+    }
+  }
+
+  compute() {
+    let remaining = this.size - this.sequence.length;
+    for (let i = 0; i < remaining; i++) {
+      const next = this.prior + this.current
+      this.sequence.push(next)
+      this.prior = this.current
+      this.current = next
+
+      this.iterations++
+    }
+  }
+
+  getSequence(length) {
+    this.setSize(length)
+
+    return this.sequence.slice(0, length)
+  }
+}
+
+module.exports = Fibonacci
